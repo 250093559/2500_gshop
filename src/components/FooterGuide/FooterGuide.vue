d322123a8c89877df3bcdee1206e69fec1d5cbfd
tangct <250093559@qq.com>
2018-08-31 21:42:28 +0800

    init gshop

---
+++ src/components/FooterGuide/FooterGuide.vue
@@ -0,0 +1,74 @@
+<template>
+	<div class="footer_guide">
+		<span class="guide_item" :class="{on:'/msite' === $route.path}" @click="goTo('/msite')">
+			<span class="item_icon">
+				<i class="iconfont icon-home"></i>
+			</span>
+			<span>外卖</span>
+		</span>
+		
+		<span class="guide_item" :class="{on:'/search' === $route.path}" @click="goTo('/search')">
+			<span class="item_icon">
+				<i class="iconfont icon-icon_search"></i>
+			</span>
+			<span>搜索</span>
+		</span>
+		
+		<span class="guide_item" :class="{on:'/order' === $route.path}" @click="goTo('/order')">
+			<span class="item_icon">
+				<i class="iconfont icon-danju"></i>
+			</span>
+			<span>订单</span>
+		</span>
+		
+		<span class="guide_item" :class="{on:'/profile' === $route.path}" @click="goTo('/profile')">
+			<span class="item_icon">
+				<i class="iconfont icon-gerenxinxi"></i>
+			</span>
+			<span>我的</span>
+		</span>
+	</div>
+</template>
+
+<script>
+export default{
+	methods:{
+	   goTo(path){
+	   	  this.$router.replace(path);  //这个方法不会向history里面添加新的记录,push会产生记录
+	   },
+	   isCurrent(path){
+	   	  return this.$route.path===path;
+	   }
+	}
+}
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+@import "../../common/stylus/mixins.styl"
+.footer_guide
+    top-border-1px(#e4e4e4)
+    position fixed
+    z-index 100
+    left 0
+    right 0
+    bottom 0
+    background-color #fff
+    width 100%
+    height 50px
+    display flex   /*弹性布局*/
+    .guide_item
+       display flex
+       flex 1         /*按比分宽*/
+       text-align center
+       flex-direction column  /*垂直显示   row水平显示   column-reverse垂直顺序相反    row-reverse水平顺序相反*/
+       align-items center     /*水平垂直居中对齐*/
+       margin 5px
+       color #999999
+       &.on
+          color #02a774
+       span
+         font-size 12px
+         margin-top 2px
+         margin-bottom 2px
+         .iconfont
+            font-size 22px         
+</style>
---
+++ src/pages/MSite/MSite.vue
@@ -0,0 +1,234 @@
+<template>
+	<div class="msite">
+		<!--首页头部-->
+		<HeaderTop :title="title">
+			<span class="header_search" slot="left">
+				<i class="iconfont icon-icon_search"></i>
+			</span>
+			<span class="header_login" slot="right">
+				<span class="header_login_text">登陆 | 注册</span>
+			</span>
+		</HeaderTop>
+		<!--首页导航-->
+		<div class="msite_nav">
+			<div class="swiper-container">
+				<div class="swiper-wrapper">
+					<div class="swiper-slide">
+						<a href="javascript:;" class="link_to_food">
+							<div class="food_container">
+								<img src="./images/nav/1.jpg">
+							</div>
+							<span>甜品饮品</span>
+						</a>
+						
+						<a href="javascript:;" class="link_to_food">
+							<div class="food_container">
+								<img src="./images/nav/2.jpg">
+							</div>
+							<span>商超便利</span>
+						</a>
+						
+						<a href="javascript:;" class="link_to_food">
+							<div class="food_container">
+								<img src="./images/nav/3.jpg">
+							</div>
+							<span>美食</span>
+						</a>
+						
+						<a href="javascript:;" class="link_to_food">
+							<div class="food_container">
+								<img src="./images/nav/4.jpg">
+							</div>
+							<span>简餐</span>
+						</a>
+						
+						<a href="javascript:;" class="link_to_food">
+							<div class="food_container">
+								<img src="./images/nav/5.jpg">
+							</div>
+							<span>新店特惠</span>
+						</a>
+						
+						<a href="javascript:;" class="link_to_food">
+							<div class="food_container">
+								<img src="./images/nav/6.jpg">
+							</div>
+							<span>准时达</span>
+						</a>
+						
+						<a href="javascript:;" class="link_to_food">
+							<div class="food_container">
+								<img src="./images/nav/7.jpg">
+							</div>
+							<span>预订早餐</span>
+						</a>
+						
+						<a href="javascript:;" class="link_to_food">
+							<div class="food_container">
+								<img src="./images/nav/8.jpg">
+							</div>
+							<span>土豪推荐</span>
+						</a>
+					</div>
+					
+					<div class="swiper-slide">
+						<a href="javascript:;" class="link_to_food">
+							<div class="food_container">
+								<img src="./images/nav/1.jpg">
+							</div>
+							<span>甜品饮品</span>
+						</a>
+						
+						<a href="javascript:;" class="link_to_food">
+							<div class="food_container">
+								<img src="./images/nav/2.jpg">
+							</div>
+							<span>商超便利</span>
+						</a>
+						
+						<a href="javascript:;" class="link_to_food">
+							<div class="food_container">
+								<img src="./images/nav/3.jpg">
+							</div>
+							<span>美食</span>
+						</a>
+						
+						<a href="javascript:;" class="link_to_food">
+							<div class="food_container">
+								<img src="./images/nav/4.jpg">
+							</div>
+							<span>简餐</span>
+						</a>
+						
+						<a href="javascript:;" class="link_to_food">
+							<div class="food_container">
+								<img src="./images/nav/5.jpg">
+							</div>
+							<span>新店特惠</span>
+						</a>
+						
+						<a href="javascript:;" class="link_to_food">
+							<div class="food_container">
+								<img src="./images/nav/6.jpg">
+							</div>
+							<span>准时达</span>
+						</a>
+						
+						<a href="javascript:;" class="link_to_food">
+							<div class="food_container">
+								<img src="./images/nav/7.jpg">
+							</div>
+							<span>预订早餐</span>
+						</a>
+						
+						<a href="javascript:;" class="link_to_food">
+							<div class="food_container">
+								<img src="./images/nav/8.jpg">
+							</div>
+							<span>土豪推荐</span>
+						</a>
+					</div>
+				</div>
+				
+				<!--Add Pagination-->
+			    <div class="swiper-pagination"></div>
+			</div>
+		</div>
+		
+		<!--首页附近商家-->
+		<div class="msite_shop_list">
+			<div class="shop_header">
+				<i class="iconfont icon-option"></i>
+				<span class="shop_header_title">附近商家</span>
+			</div>
+			<ShopList></ShopList>
+		</div>
+	</div>
+</template>
+
+<script>
+import HeaderTop from '@/components/HeaderTop/HeaderTop'
+import ShopList from '@/components/ShopList/ShopList'
+import Swiper from 'swiper'   //swiper插件只引入这个和下面那个CSS就可以用了
+import 'swiper/dist/css/swiper.min.css'
+export default{
+	data(){
+		return {
+			title:'佛山市禅城区港口路碧桂园城市花园'
+		}
+	},
+	components:{
+		HeaderTop,
+		ShopList
+	},
+	mounted(){
+		//创建一个Swiper实例对象，来实现轮播 教程详细https://www.swiper.com.cn/usage/index.html
+		new Swiper ('.swiper-container', {
+            loop: true,   //循环轮播
+            // 如果需要分页器
+            pagination: {
+                el: '.swiper-pagination',
+            }
+        })
+	}
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import "../../common/stylus/mixins.styl"
+.msite 
+   width 100%
+   .msite_nav
+        bottom-border-1px(#e4e4e4)
+        margin-top 45px
+        height 200px
+        background #fff
+        .swiper-container
+            width 100%
+            height 100%
+            .swiper-wrapper
+                width 100%
+                height 100%
+                .swiper-slide
+                    display flex
+                    justify-content center
+                    align-items center
+                    flex-wrap wrap
+                    width 100%
+                    height 100%
+                    .link_to_food
+                        width 25%
+                        .food_container
+                            display block
+                            width 100%
+                            text-align center
+                            padding-bottom 10px
+                            img
+                               display inline-block
+                               width 50px
+                               height 50px
+                        span
+                            display block
+                            width 100%
+                            text-align center
+                            font-size 13px
+                            color #666
+                            padding-bottom 20px;  /*修改swiper对称问题*/
+            .swiper-pagination
+                >span.swiper-pagination-bullet-active
+                    background #02a774        
+   .msite_shop_list
+       bottom-border-1px(#e4e4e4)
+       margin-top 10px
+       background #fff
+       .shop_header
+            padding 10px 10px 0
+            .icon-option
+                margin-left 5px
+                color #999
+            .shop_header_title
+                color #999
+                font-size 16px
+                line-height 20px
+       
+</style>
